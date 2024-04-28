de9cfa2521b5c2d415e567153e2349c0fa7d89e1
Martin Dymenstein <martin@dymenstein.com>
2024-04-28 14:04:07 -0300

    init

---
+++ src/main/java/com/mdymen/jpaauxtable/JpaAuxTableApplication.java
@@ -0,0 +1,13 @@
+package com.mdymen.jpaauxtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAuxTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAuxTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdymen/jpaauxtable/JpaAuxTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdymen.jpaauxtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAuxTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
